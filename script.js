80d5489f6e6e1c3398e4e1c6f42f541c0f5feada
rafaelhenrique-dev <107144272+rafaelhenrique-dev@users.noreply.github.com>
2025-05-11 10:03:14 -0300

    Adicionando arquivos base do site

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function mostrarMensagem() {
+  const msg = document.getElementById("mensagem");
+  msg.innerText = "Quando você quer alguma coisa todo o universo conspira para que você realize o seu desejo. — Paulo Coelho";
+  msg.classList.add("frase-simples");
+}
